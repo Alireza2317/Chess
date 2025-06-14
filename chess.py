165a17f2cb440f96b12257b042019c7be57765d0
Alireza <alireza.alavi237@gmail.com>
2025-06-15 03:21:36 +0330

    created Coordinate class; replaced with str coordinates

--- chess.py
+++ chess.py
@@ -24,6 +24,29 @@ class Pawn:
 Piece: TypeAlias = King | Queen | Rook | Bishop | Knight | Pawn
 
 
+class Coordinate:
+	def __init__(self, coordinate: str):
+		file: str = str(coordinate[0]).lower()
+		rank: str = str(coordinate[1])
+
+		if (len(coordinate) != 2) or (file not in 'abcdefgh') or (rank not in '12345678'):
+			raise ValueError('Invalid chess coordinate!')
+
+
+		self.file: str = file
+		self.rank: str = rank
+
+
+	@property
+	def regular(self) -> tuple[int, int]:
+		""" converts chess coordinates like 'a1' to regular matrix coordinates. """
+
+		row: int = int(self.rank) - 1
+		col: int = 'abcdefgh'.index(self.file)
+
+		return row, col
+
+
 class Board:
 	def __init__(self):
 		# the arrangement of these lists is such that the first row is
@@ -35,34 +58,18 @@ class Board:
 		]
 
 
-	def chess2regular_coordinate(self, chess_coordinate: str) -> tuple[int, int]:
-		""" converts chess coordinates like 'a1' to regular matrix coordinates. """
-		coord = chess_coordinate
-
-		file: str = str(coord[0]).lower()
-		rank: str = str(coord[1])
-
-		if (len(coord) != 2) or (file not in 'abcdefgh') or (rank not in '12345678'):
-			raise ValueError('Invalid chess coordinate!')
-
-		row: int = int(rank) - 1
-		col: int = 'abcdefgh'.index(file)
-
-		return row, col
-
-
-	def put(self, piece: Piece | None, chess_coordinate: str) -> None:
+	def put(self, piece: Piece | None, chess_coordinate: Coordinate) -> None:
 		"""
-		takes a chess coordinate like 'a1' and puts
-		the given piece in the appropriate location
+		takes a chess coordinate and puts the given piece
+		in the appropriate location
 		"""
 
-		row, col = self.chess2regular_coordinate(chess_coordinate)
+		row, col = chess_coordinate.regular
 
 		self.board[row][col] = piece
 
 
-	def remove(self, chess_coordinate: str) -> None:
+	def remove(self, chess_coordinate: Coordinate) -> None:
 		""" removes the piece(if any) from the given coordinate. """
 		self.put(None, chess_coordinate)
 
