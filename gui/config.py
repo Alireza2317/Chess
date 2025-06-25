98d03e0f93cdab7460fab3b614a76c089310f71b
Alireza <alireza.alavi237@gmail.com>
2025-06-26 01:59:16 +0330

    added some gui config

--- gui/config.py
+++ gui/config.py
@@ -5,7 +5,8 @@ Color: TypeAlias = tuple[int, int, int]
 
 @dataclass
 class ChessGUIConfig:
-	dimensions: tuple[int, int] = (824, 824)
+	coordinates_width: int = 24
+	dimensions: tuple[int, int] = (800, 800)
 	square_size: int = 100
 	padding: int = 20
 	fps: int = 10
@@ -13,6 +14,8 @@ class ChessGUIConfig:
 	bg_color: Color = (20, 20, 20)
 	black_color: Color = (200, 100, 100)
 	white_color: Color = (80, 50, 50)
+	coordinates_text_color: Color = (0, 0, 0)
+	coordinates_bg_color: Color = (150, 150, 150)
 	pieces_theme: str = 'neo_wood'
 
 	font_size: int = 14
