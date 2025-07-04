034b4c0ce6b32eb3ade3aeafed5ece3edbcf96f5
Alireza <alireza.alavi237@gmail.com>
2025-07-04 13:51:59 +0330

    made an RGBColor type in gui.config

--- gui/config.py
+++ gui/config.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import TypeAlias
 
-Color: TypeAlias = tuple[int, int, int]
+RGBColor: TypeAlias = tuple[int, int, int]
 
 @dataclass
 class ChessGUIConfig:
@@ -11,12 +11,12 @@ class ChessGUIConfig:
 	padding: int = 20
 	fps: int = 10
 
-	bg_color: Color = (20, 20, 20)
-	black_color: Color = (200, 100, 100)
-	white_color: Color = (80, 50, 50)
-	valid_color: Color = (75, 212, 84)
-	coordinates_text_color: Color = (0, 0, 0)
-	coordinates_bg_color: Color = (150, 150, 150)
+	bg_color: RGBColor = (20, 20, 20)
+	black_color: RGBColor = (200, 100, 100)
+	white_color: RGBColor = (80, 50, 50)
+	valid_color: RGBColor = (75, 212, 84)
+	coordinates_text_color: RGBColor = (0, 0, 0)
+	coordinates_bg_color: RGBColor = (150, 150, 150)
 	pieces_theme: str = 'neo_wood'
 
 	font_size: int = 14
