913a36190546c8c0be1e8bd60a2336eb8b5ff7f0
Alireza <alireza.alavi237@gmail.com>
2025-07-05 01:37:19 +0330

    updated gui config

--- gui/config.py
+++ gui/config.py
@@ -12,9 +12,12 @@ class ChessGUIConfig:
 	fps: int = 10
 
 	bg_color: RGBColor = (20, 20, 20)
-	black_color: RGBColor = (200, 100, 100)
-	white_color: RGBColor = (80, 50, 50)
-	valid_color: RGBColor = (75, 212, 84)
+	#black_color: RGBColor = (200, 100, 100)
+	#white_color: RGBColor = (80, 50, 50)
+	black_color: RGBColor = (168, 84, 13)
+	white_color: RGBColor = (237, 165, 104)
+	#valid_color: RGBColor = (75, 212, 84)
+	valid_color: RGBColor = (85, 195, 144)
 	coordinates_text_color: RGBColor = (0, 0, 0)
 	coordinates_bg_color: RGBColor = (150, 150, 150)
 	pieces_theme: str = 'neo_wood'
