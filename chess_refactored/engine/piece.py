3d5a928d47f1a8873870b0cecdef79aea5de4bc9
Alireza <alireza.alavi237@gmail.com>
2025-07-13 20:27:37 +0330

    checking occupancy of square before adding pieces

--- chess_refactored/engine/piece.py
+++ chess_refactored/engine/piece.py
@@ -34,10 +34,13 @@ class Piece(ABC):
 		self.owner: Player = player
 		self.coordinate: Coordinate = coordinate
 
+		# put the piece on the board if square is not occupied
+		if self.owner.board[coordinate].piece:
+			raise ValueError(f'There is already a piece on {coordinate}!')
+		self.owner.board.place_piece(self, coordinate)
+
 		# add the piece to player's pieces
 		self.owner.pieces.add(self)
-		# put the piece on the board
-		self.owner.board.place_piece(self, coordinate)
 
 	@property
 	@abstractmethod
