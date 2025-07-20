12f32924d668d2e4bd0b50e1551dbd358aae32a8
Alireza <alireza.alavi237@gmail.com>
2025-07-21 03:24:09 +0330

    checking validity of en passant moves

--- chess/engine/en_passant.py
+++ chess/engine/en_passant.py
@@ -2,6 +2,9 @@ from chess.engine.core import Coordinate, Direction
 from chess.engine.piece import Piece, PieceType
 from chess.engine.pieces.pawn import Pawn
 from chess.engine.moves.move import Move
+from chess.engine.moves.simulator import MoveSimulator
+from chess.engine.moves.factory import create_move
+
 
 def add_en_passant_if_possible(pawn: Piece, last_move: Move | None) -> None:
 	if not isinstance(pawn, Pawn):
@@ -34,4 +37,7 @@ def add_en_passant_if_possible(pawn: Piece, last_move: Move | None) -> None:
 		op_pawn.coordinate.file, pawn.en_passant_target_rank
 	)
 
-	pawn.legal_moves.add(target_ep)
+	move: Move = create_move(pawn, target_ep, simulation=True)
+	with MoveSimulator(pawn.owner, move):
+		if not pawn.owner.is_in_check():
+			pawn.legal_moves.add(target_ep)
