ce57a0a1a0cc33cc315c4f36726ca05da982d218
Alireza Alavi <alireza.alavi237@gmail.com>
2025-07-21 14:57:14 +0330

    implemented fen loader's setup pieces

--- chess/game/cli_chess.py
+++ chess/game/cli_chess.py
@@ -1,13 +1,8 @@
 import enum
 from chess.engine.core import Coordinate, Color
-from chess.engine.pieces.bishop import Bishop
-from chess.engine.pieces.king import King
-from chess.engine.pieces.knight import Knight
-from chess.engine.pieces.pawn import Pawn
-from chess.engine.pieces.queen import Queen
-from chess.engine.pieces.rook import Rook
 from chess.engine.piece import Piece, PieceType
 from chess.game.game import Game
+from chess.game.fen import fen_loader
 
 MoveDetail = tuple[Coordinate, Coordinate, PieceType | None]
 
@@ -21,53 +16,7 @@ def clear_console() -> None:
 	print("\033[H\033[J", end="")
 
 def classic_setup() -> Game:
-	g: Game = Game()
-
-	King(g.white, Coordinate.from_str("e1"))
-	King(g.black, Coordinate.from_str("e8"))
-
-	Queen(g.white, Coordinate.from_str("d1"))
-	Queen(g.black, Coordinate.from_str("d8"))
-
-	Rook(g.white, Coordinate.from_str("a1"))
-	Rook(g.white, Coordinate.from_str("h1"))
-	Rook(g.black, Coordinate.from_str("a8"))
-	Rook(g.black, Coordinate.from_str("h8"))
-
-	Knight(g.white, Coordinate.from_str("b1"))
-	#Knight(g.white, Coordinate.from_str("g1"))
-	Knight(g.black, Coordinate.from_str("b8"))
-	Knight(g.black, Coordinate.from_str("g8"))
-
-	Bishop(g.white, Coordinate.from_str("c1"))
-	#Bishop(g.white, Coordinate.from_str("f1"))
-	Bishop(g.black, Coordinate.from_str("c8"))
-	Bishop(g.black, Coordinate.from_str("f8"))
-
-	Pawn(g.white, Coordinate.from_str("a2"))
-	Pawn(g.white, Coordinate.from_str("b2"))
-	Pawn(g.white, Coordinate.from_str("c2"))
-	Pawn(g.white, Coordinate.from_str("d2"))
-	#Pawn(g.white, Coordinate.from_str("e2"))
-	Pawn(g.white, Coordinate.from_str("c6"))
-	Pawn(g.white, Coordinate.from_str("f2"))
-	#Pawn(g.white, Coordinate.from_str("f3"))
-	Pawn(g.white, Coordinate.from_str("g2"))
-	#Pawn(g.white, Coordinate.from_str("g4"))
-	Pawn(g.white, Coordinate.from_str("h2"))
-
-	#Pawn(g.black, Coordinate.from_str("a7"))
-	Pawn(g.black, Coordinate.from_str("a5"))
-	Pawn(g.black, Coordinate.from_str("b7"))
-	#Pawn(g.black, Coordinate.from_str("c7"))
-	#Pawn(g.black, Coordinate.from_str("d7"))
-	Pawn(g.black, Coordinate.from_str("e7"))
-	#Pawn(g.black, Coordinate.from_str("e6"))
-	Pawn(g.black, Coordinate.from_str("f7"))
-	Pawn(g.black, Coordinate.from_str("g7"))
-	Pawn(g.black, Coordinate.from_str("h7"))
-
-	return g
+	return fen_loader('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR - - - - -')
 
 def handle_input(
 	game: Game
---
+++ chess/game/fen.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from chess.engine.core import Coordinate
+from chess.engine.pieces.king import King
+from chess.engine.pieces.queen import Queen
+from chess.engine.pieces.rook import Rook
+from chess.engine.pieces.bishop import Bishop
+from chess.engine.pieces.knight import Knight
+from chess.engine.pieces.pawn import Pawn
+from chess.game.game import Game
+if TYPE_CHECKING:
+	from chess.engine.player import Player
+
+def fen_loader(fen: str) -> Game:
+	fields: list[str] = fen.split()
+	if len(fields) != 6:
+		raise ValueError('Invalid FEN string!')
+
+	pieces, turn, castle, ept, halfmove, fullmove = fields
+
+	game: Game = Game()
+
+	setup_pieces(game, pieces)
+
+	return game
+
+def setup_pieces(game: Game, fen_pieces_part: str) -> None:
+	# fen_pieces_part is the first field of FEN
+	p_placements_ranks: list[str] = fen_pieces_part.split('/')
+	if len(p_placements_ranks) != 8:
+		raise ValueError('Invalid FEN string!')
+
+	for rank, rank_pieces in zip(Coordinate.RANKS[::-1], p_placements_ranks):
+		file_idx: int = 0
+		file: str = Coordinate.FILES[file_idx]
+		for piece in rank_pieces:
+			# can be 'KQRBNPkqrbnp' or a number between 1 and 8
+			# the number denotes the number of consecutive empty squares
+			if piece.isdigit():
+				file_idx += int(piece)
+				continue
+
+			player: Player
+			if piece.isupper():
+				player = game.white
+			else:
+				player = game.black
+
+			file = Coordinate.FILES[file_idx]
+			coord = Coordinate(file, rank)
+
+			piece_map: dict[str, type] = {
+				'k': King,
+				'q': Queen,
+				'r': Rook,
+				'b': Bishop,
+				'n': Knight,
+				'p': Pawn,
+			}
+
+			piece_t: type = piece_map[piece.lower()]
+			piece_t(player, coord)
+
+			file_idx += 1
+
