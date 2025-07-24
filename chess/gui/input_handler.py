a8590fd78dfcf66efc19de13658ce66a22c01d12
Alireza <alireza.alavi237@gmail.com>
2025-07-24 16:54:55 +0330

    defined get_coord_from_mouse function

---
+++ chess/gui/input_handler.py
@@ -0,0 +1,14 @@
+from chess.engine.core import Coordinate
+from gui.config import cfg
+
+def get_coord_from_mouse(pos: tuple[int, int]) -> Coordinate | None:
+    x, y = pos
+    file_idx: int = x // cfg.square_size
+    rank_idx: int = 7 - (y // cfg.square_size)
+
+    if 0 <= file_idx < 8 and 0 <= rank_idx < 8:
+        file: str = Coordinate.FILES[file_idx]
+        rank: str = Coordinate.RANKS[rank_idx]
+        return Coordinate(file, rank)
+
+    return None
