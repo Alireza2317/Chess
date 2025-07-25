45effec2ec8eeba71c8932346cfad969a7fc1784
Alireza <alireza.alavi237@gmail.com>
2025-07-25 15:55:40 +0330

    fixed gui_loop function; functional gui

--- chess/gui/gui_game.py
+++ chess/gui/gui_game.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from chess.engine.core import Coordinate
+from chess.engine.piece import Piece
 from chess.game.game import Game
 from chess.engine.setup import classic_setup
 from chess.gui.renderer import Renderer
@@ -11,44 +12,73 @@ screen = pg.display.set_mode(cfg.dimensions)
 pg.display.set_caption("Chess")
 
 
+def handle_left_click(
+		game: Game, current_selected: Coordinate | None
+) -> Coordinate | None :
+	new_selected: Coordinate | None = None
+	coord: Coordinate | None = get_coord_from_mouse(pg.mouse.get_pos())
+	if not coord:
+		return None
+	if current_selected:
+		piece: Piece | None = game.board[current_selected].piece
+		# allow move if selected square is a piece of current player
+		if piece is not None and piece.owner == game.current_player:
+			if coord in piece.legal_moves:
+				try:
+					game.play_turn(current_selected, coord)
+				except Exception as e:
+					print(f'Error while moving piece: {e}')
+				new_selected = None
+			elif coord == current_selected:
+				new_selected = None  # Deselect if clicking same square
+			else:
+				other_piece: Piece | None = game.board[coord].piece
+				# Basically selecting another of our own piece, while
+				# we have a piece selected already
+				if other_piece is not None and other_piece.owner == game.current_player:
+					new_selected = coord  # Select it
+
+		else: # selected empty square or enemy piece
+			new_selected = None
+	else: # No selected piece yet
+		# Only select if square has a piece belonging to current player
+		piece = game.board[coord].piece
+		if piece and piece.owner == game.current_player:
+			game.update_legal_moves()
+			new_selected = coord
+		else:
+			new_selected = None
+
+	return new_selected
+
 def gui_loop(game: Game) -> None:
 	renderer: Renderer = Renderer(game.board, screen)
 	selected: Coordinate | None = None
-
-	running: bool = True
-	while running:
-		screen.fill(cfg.bg_color)
+	screen.fill(cfg.bg_color)
+	while True:
 		renderer.draw_board()
 		renderer.draw_pieces()
 
+		# Highlight legal moves for selected piece
 		if selected:
 			piece: Piece | None = game.board[selected].piece
-			if piece:
-				renderer.highlight_squares({selected, *piece.legal_moves})
+			if piece and piece.owner == game.current_player:
+				renderer.highlight_squares(piece.legal_moves)
+			else:
+				selected = None
 
-		pg.display.flip()
+		pg.display.update()
 
 		for event in pg.event.get():
 			if event.type == pg.QUIT:
-				running = False
-
-			elif event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-				coord: Coordinate = get_coord_from_mouse(pg.mouse.get_pos())
-				if not coord:
-					continue
-
-				piece = game.board[coord].piece
-
-				if selected and piece and coord in piece.legal_moves:
-					game.play_turn(selected, coord)
-					selected = None
-				elif piece and piece.owner == game.current_player:
-					game.update_legal_moves()
-					selected = coord
-				else:
-					selected = None
-
-	pg.quit()
+				pg.quit()
+				exit()
+			elif event.type == pg.MOUSEBUTTONDOWN:
+				print(event.button)
+				if event.button == 1: # left click
+					selected = handle_left_click(game, selected)
+				elif event.button == 3: # right click
+					pass
 
 if __name__ == '__main__':
 	game = classic_setup()
